c0b7cf10051dbc23878036617bccd38e58640925
takumab <takumab@gmail.com>
2019-08-22 14:26:12 +0100

    Add first test and override getLoggedUser()

--- src/test/java/com/tripservicekata/trip/TripServiceShould.java
+++ src/test/java/com/tripservicekata/trip/TripServiceShould.java
@@ -1,4 +1,23 @@
 package com.tripservicekata.trip;
 
+import com.tripservicekata.exception.UserNotLoggedInException;
+import com.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TripServiceShould {
+    @Test
+    void throw_exception_when_user_not_logged_in() {
+        TripService testtableTripService = new TesttableTripService();
+        User user = new User();
+        assertThrows(UserNotLoggedInException.class, () -> testtableTripService.getTripsByUser(user));
+    }
+
+    private class TesttableTripService extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return null;
+        }
+    }
 }
