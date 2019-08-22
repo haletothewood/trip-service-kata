d036788c7952fa2a3717ae0f289146fe171e7156
David Halewood <davidnhalewood@hotmail.com>
2019-08-22 15:01:24 +0100

    Add test for no trips for user with no friends

--- src/test/java/com/tripservicekata/trip/TripServiceShould.java
+++ src/test/java/com/tripservicekata/trip/TripServiceShould.java
@@ -3,21 +3,41 @@ package com.tripservicekata.trip;
 import com.tripservicekata.exception.UserNotLoggedInException;
 import com.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 public class TripServiceShould {
+
+    private static final User GUEST = null;
+    private static final User REGISTERED_USER = mock(User.class);
+    private User loggedInUser;
+    private TripService tripService = new TestableTripService();
+
     @Test
     void throw_exception_when_user_not_logged_in() {
-        TripService testtableTripService = new TesttableTripService();
-        User user = new User();
-        assertThrows(UserNotLoggedInException.class, () -> testtableTripService.getTripsByUser(user));
+        loggedInUser = GUEST;
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(loggedInUser));
     }
 
-    private class TesttableTripService extends TripService {
+
+    @Test
+    void provide_no_trips_for_a_user_with_no_friends() {
+        loggedInUser = REGISTERED_USER;
+        when(REGISTERED_USER.getFriends()).thenReturn(new ArrayList<>());
+
+        assertThat(tripService.getTripsByUser(REGISTERED_USER).size()).isEqualTo(0);
+    }
+
+    private class TestableTripService extends TripService {
         @Override
         protected User getLoggedUser() {
-            return null;
+            return loggedInUser;
         }
     }
 }
