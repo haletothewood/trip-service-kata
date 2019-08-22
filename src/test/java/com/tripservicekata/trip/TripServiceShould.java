3dab916d555963d5bec48017040236e7a611187e
David Halewood <davidnhalewood@hotmail.com>
2019-08-22 15:01:24 +0100

    Add test for no trips for user with no friends
    Add test for returning trips for a logged in user with a friend

--- src/test/java/com/tripservicekata/trip/TripServiceShould.java
+++ src/test/java/com/tripservicekata/trip/TripServiceShould.java
@@ -4,20 +4,58 @@ import com.tripservicekata.exception.UserNotLoggedInException;
 import com.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class TripServiceShould {
+
+    private static final User GUEST = null;
+    private static final User REGISTERED_USER = mock(User.class);
+    private User loggedInUser;
+    private TripService tripService = new TestableTripService();
+    private Trip TO_LONDON = new Trip();
+    private List<Trip> trips = new ArrayList<>();
+
     @Test
     void throw_exception_when_user_not_logged_in() {
-        TripService testtableTripService = new TesttableTripService();
-        User user = new User();
-        assertThrows(UserNotLoggedInException.class, () -> testtableTripService.getTripsByUser(user));
+        loggedInUser = GUEST;
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(loggedInUser));
     }
 
-    private class TesttableTripService extends TripService {
+
+    @Test
+    void provide_no_trips_for_a_user_with_no_friends() {
+        loggedInUser = REGISTERED_USER;
+        User user = new User();
+        assertThat(tripService.getTripsByUser(user).size()).isEqualTo(0);
+    }
+
+    @Test
+    void provides_trips_for_a_user_with_friends() {
+        loggedInUser = REGISTERED_USER;
+        User user = new User();
+        user.addFriend(loggedInUser);
+        user.addTrip(TO_LONDON);
+        trips = new ArrayList<Trip>(){{
+            add(TO_LONDON);
+        }};
+
+        assertThat(tripService.getTripsByUser(user)).contains(TO_LONDON);
+    }
+
+    private class TestableTripService extends TripService {
         @Override
         protected User getLoggedUser() {
-            return null;
+            return loggedInUser;
+        }
+
+        @Override
+        protected List<Trip> findTripsByUser(User user) {
+            return trips;
         }
     }
 }
